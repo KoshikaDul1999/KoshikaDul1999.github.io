293c066a1b11338739ecb58912328b090f1f2f53
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-08-07 14:00:11 +0530

    create components and their files

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -2,4 +2,4 @@ import ReactDom from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDom.render(<App/>, document.querySelector("root"))
+ReactDom.render(<App/>, document.querySelector("#root"))
