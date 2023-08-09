76465074dfb3b393bc25270bb85c88f63c37189f
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-08-09 16:13:06 +0530

    Update About.jsx

--- my-portfolio/src/components/about/About.jsx
+++ my-portfolio/src/components/about/About.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import "./about.css"
+import ME from '../../assests/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+          As a software engineering undergraduate, I am passionate
+          and driven, with a strong foundation in computer science
+          and programming. My deep understanding of software
+          development principles, exceptional problem-solving skills,
+          and quick adaptability to new technologies make me
+          valuable to any software development team. Proficient in
+          independent and collaborative work, I am eager to expand
+          my knowledge and contribute to innovative solutions in the
+          field of software engineering.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
