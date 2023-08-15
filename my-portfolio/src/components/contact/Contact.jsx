3d81508e90fefa600dbf4296aa33ba04c48da24a
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-08-16 00:05:23 +0530

    update send message part

--- my-portfolio/src/components/contact/Contact.jsx
+++ my-portfolio/src/components/contact/Contact.jsx
@@ -3,8 +3,19 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_1rgkocn', 'template_8s8uyh8', form.current, 'YOUR_PUBLIC_KEY')
+    
+    e.target.reset()
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -32,7 +43,7 @@ const Contact = () => {
           </article>
         </div>
         {/* End of the contact options */}
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type='text' name='name' placeholder='Your Full Name' required />
           <input type='email' name='email' placeholder='Your Email' required />
           <textarea name='message' rows="7" placeholder='Your Message' required ></textarea>
