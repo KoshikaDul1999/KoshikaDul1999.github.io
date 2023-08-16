0626700b83b5fcf1568b3af57952818ddbcffc5a
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-08-16 20:38:12 +0530

    update footer.css

--- my-portfolio/src/components/footer/Footer.jsx
+++ my-portfolio/src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href='#' className='footer__logo'>KOSHIKA</a>
+      <a href='#' className='footer__logo'>Koshika Dulanjana</a>
 
       <ul className='permalinks'>
         <li><a href='#'>Home</a></li>
