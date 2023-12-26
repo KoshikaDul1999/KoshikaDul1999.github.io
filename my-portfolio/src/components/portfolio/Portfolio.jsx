a0228543d76204dfe788dbd92658c086a46466e9
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-12-26 12:16:41 +0530

    updated

--- my-portfolio/src/components/portfolio/Portfolio.jsx
+++ my-portfolio/src/components/portfolio/Portfolio.jsx
@@ -6,41 +6,48 @@ import IMG3 from '../../assests'
 import IMG4 from '../../assests'
 import IMG5 from '../../assests'
 import IMG6 from '../../assests/portfolio6.png'
+import IMG7 from '../../assests/portfolio7.png'
 
 const data = [
   {
     id: 1,
     image:IMG1,
-    title: 'ndjd udvuefv uidvevui uibv fbvj',
+    title: 'Rockvin Cafe Web - ReactJS & NodeJS',
     github: 'https://github.com/KoshikaDul1999',
   },
   {
     id: 2,
     image:IMG2,
-    title: 'ndjd udvuefv uidvevui uibv fbvj',
+    title: 'Hostal Management System - Angular & SpringBoot',
     github: 'https://github.com/KoshikaDul1999',
   },
   {
     id: 3,
     image:IMG3,
-    title: 'ndjd udvuefv uidvevui uibv fbvj',
+    title: 'Gym Web Site - ReactJS',
     github: 'https://github.com/KoshikaDul1999',
   },
   {
     id: 4,
     image:IMG4,
-    title: 'ndjd udvuefv uidvevui uibv fbvj',
+    title: 'Chat Application - Java',
     github: 'https://github.com/KoshikaDul1999',
   },
   {
     id: 5,
     image:IMG5,
-    title: 'ndjd udvuefv uidvevui uibv fbvj',
+    title: 'Pet Shop - PHP',
     github: 'https://github.com/KoshikaDul1999',
   },
   {
     id: 6,
     image:IMG6,
+    title: 'Haai Shopping System - PHP',
+    github: '',
+  },
+  {
+    id: 7,
+    image:IMG7,
     title: 'Car Rental System - Java',
     github: 'https://github.com/BK-KAVIYA/GetFarCar-CarRentalSystem',
   },
