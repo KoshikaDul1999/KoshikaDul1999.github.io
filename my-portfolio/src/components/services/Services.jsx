8e5585353146e3f90486c2b52f0f0d71ee696aa9
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-03-24 11:34:53 +0530

    Update Services.jsx

--- my-portfolio/src/components/services/Services.jsx
+++ my-portfolio/src/components/services/Services.jsx
@@ -11,107 +11,75 @@ const Services = () => {
       <div className='container services__container'>
         <article className='service'>
           <div className='service__head'>
-            <h3>UI/UX Design</h3>
-          </div>
-
-          <ul className='service__list'>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
-            </li>
-          </ul>
-        </article>
-        {/* END OF UI/UX */}
-        <article className='service'>
-          <div className='service__head'>
             <h3>Web Development</h3>
           </div>
 
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+              <p>Proficient in a variety of technologies including Spring Boot, Angular, React JS, and Node JS.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+              <p>Skilled in both front-end and back-end development, ensuring holistic understanding and implementation of web projects.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+              <p>Expertise in leveraging Spring Boot for robust backend functionalities and Angular, and React JS for dynamic front-end development.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+              <p>Committed to delivering high-quality solutions that meet the requirements of modern web applications.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI/UX */}
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Frontend Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Specialized in creating engaging user interfaces using technologies such as React JS, Angular, HTML, CSS, and JavaScript.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+              <p>Possesses a keen eye for design and a focus on user experience to ensure intuitive and responsive web applications.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+              <p>Proficient in building with React JS for its component-based architecture or utilizing Angular for its robust framework.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+              <p>Passionate about translating designs into seamless digital experiences that captivate users.</p>
             </li>
           </ul>
         </article>
         {/* END OF WEB DEVELOPMENT */}
         <article className='service'>
           <div className='service__head'>
-            <h3>Content Creation</h3>
+            <h3>Backend Development</h3>
           </div>
 
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+              <p>Experienced in building scalable and efficient backend systems using Spring Boot, Node JS, Java, and Python.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+              <p>Specializes in developing resilient APIs, managing databases, and ensuring smooth server-side operations.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+              <p>Proficient in utilizing Spring Boot's extensive libraries and frameworks for Java-based applications.</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+              <p>Versatile in leveraging the versatility of Node JS and Python for backend development, ensuring reliability and high performance.</p>
             </li>
           </ul>
         </article>
