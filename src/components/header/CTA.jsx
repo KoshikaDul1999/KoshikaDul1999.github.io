3caab664fa158c555575727700ec9532840c38e0
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-03-27 11:48:11 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 import CV from "../../assests/cv.pdf"
 
 const CTA = () => {
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = CV;
+    link.setAttribute('download', 'G.Koshika_Dulanjana_CV.pdf'); 
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+  };
+
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'>Download CV</a>
-      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+      <button onClick={handleDownload} download className='btn btn-primary'>Download CV</button>
+      <a href="#contact" className='btn'>Let's Talk</a>
     </div>
   )
 }
