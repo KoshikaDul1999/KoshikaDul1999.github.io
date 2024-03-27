5cc675daefec1623a0918ffe83723a5cfd0ed72c
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-03-27 11:47:57 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,8 +14,8 @@ const Footer = () => {
         <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Experience</a></li>
         <li><a href='#services'>Services</a></li>
-        <li><a href='#portfolio'>Portfolio</a></li>
-        <li><a href='#testimonials'>Testimonials</a></li>
+        <li><a href='#portfolio'>Projects</a></li>
+        <li><a href='#testimonials'>Recommendations</a></li>
         <li><a href='#contact'>Contact</a></li>
       </ul>
 
